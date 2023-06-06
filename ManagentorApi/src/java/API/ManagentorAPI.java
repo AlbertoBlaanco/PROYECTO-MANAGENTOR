f5a34120f2afad0647abe9f200fdbf64a1099bdb
AlbertoBlaanco <albertoblanco633@gmail.com>
2023-06-06 22:50:52 +0200

    Pulling

--- ManagentorApi/src/java/API/ManagentorAPI.java
+++ ManagentorApi/src/java/API/ManagentorAPI.java
@@ -193,10 +193,6 @@ public class ManagentorAPI {
         PropiedadDAO propiedadDAO = new PropiedadDAO();
         propiedadDAO.addPropiedad(propiedad);
         return "finished";
-        //AlquiladosDAO alquiladosDAO = new AlquiladosDAO();
-        //ArrayList<Alquilados> lstClientes = alquiladosDAO.findAll(null);
-        //return Alquilados.fromArrayListToJson(lstClientes);
-
     }
 
     @POST
@@ -254,15 +250,12 @@ public class ManagentorAPI {
     public String uploadImage(String base64Image) {
         Gson gson = new Gson();
         try {
-            // Decodificar la imagen en Base64
-            /*for (int i = 0; i < lstbase64Image.size(); i++) {
-                
-            }*/
+
             byte[] imageBytes = DatatypeConverter.parseBase64Binary(base64Image);
 
             // Crear un directorio para guardar las imágenes
             String imagesDirectory = "E:\\Escritorio\\Insti\\2año\\Insti\\PHP\\www\\PruebaImagenesAndroid";
-            //http://192.168.1.36//PruebaImagenesAndroid/image_1684074786042.jpg
+
             String urlBBDD = "http://192.168.1.42//PruebaImagenesAndroid/";
 
             File directory = new File(imagesDirectory);
@@ -313,7 +306,7 @@ public class ManagentorAPI {
 
         return Usuario.fromArrayListToJsonOne(usuarioRespuesta);
     }
-    
+
     @POST
     @Path("/insertUser")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -327,135 +320,4 @@ public class ManagentorAPI {
         return "Hecho";
     }
 
-    /*
-    ////////////COSAS DE LA API DE GLOVO//////////////
-    @GET
-    @Path("/filtr_usu/{tipo}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getFiltroComida(@PathParam("tipo") String tipo) {
-        //TODO return proper representation object
-
-        RestauranteDAO restauranteDAO = new RestauranteDAO();
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNOMBRE("");
-        restaurante.setIMAGEN("");
-        if (tipo.equals("Tipo")) {
-            restaurante.setTIPO("");
-        } else {
-            restaurante.setTIPO(tipo);
-        }
-
-        ArrayList<Restaurante> lstRestaurantes = restauranteDAO.findAllAk(restaurante);
-        return Restaurante.fromArrayListToJson(lstRestaurantes);
-    }
-
-    @GET
-    @Path("/ver_ficha/{idRestaurante}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getFichaRestaurante(@PathParam("idRestaurante") int idRestaurante) {
-        //TODO return proper representation object
-
-        RestauranteDAO restauranteDAO = new RestauranteDAO();
-        Restaurante restaurante = new Restaurante();
-        if (idRestaurante != 0) {
-            restaurante.setID_RESTAURANTE(idRestaurante);
-        } else {
-
-        }
-
-        Restaurante restauranteRespuesta = restauranteDAO.findOne(restaurante);
-        return Restaurante.fromArrayListToJsonOnly(restauranteRespuesta);
-    }
-
-    @GET
-    @Path("/ver_carta/{idRestaurante}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getCartaRestaurante(@PathParam("idRestaurante") int idRestaurante) {
-        //TODO return proper representation object
-
-        PlatoDAO platoDAO = new PlatoDAO();
-        Plato plato = new Plato();
-        if (idRestaurante != 0) {
-            plato.setId_restaurante(idRestaurante);
-        } else {
-
-        }
-
-        ArrayList<Plato> cartaRespuesta = platoDAO.findRestaurantesVentas(plato);
-        return Plato.fromArrayListToJson(cartaRespuesta);
-    }
-
-    @GET
-    @Path("/comprar/{total}/{idUser}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String comprarCarro(@PathParam("total") String total, @PathParam("idUser") int id_Usuario) {
-        //TODO return proper representation object
-
-        Pedido pedido = new Pedido();
-        PedidoDAO pedidoDAO = new PedidoDAO();
-        Date date = new Date();
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
-
-        pedido.setId_usuario(id_Usuario);
-        pedido.setPrecio_total(total);
-        pedido.setFecha(dateFormat.format(date));
-        pedidoDAO.add(pedido);
-        return "Hecho";
-    }
-
-    @GET
-    @Path("/register/{nombre}/{password}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Usuario register(@PathParam("nombre") String nombre, @PathParam("password") String password) {
-        UsuarioDAO usuarioDao = new UsuarioDAO();
-        Usuario usuario = new Usuario();
-        ArrayList<Usuario> usuarios = usuarioDao.findAll(null);
-        for (Usuario usuarioRespuesta : usuarios) {
-            if (usuarioRespuesta.getNombre().equals(nombre) && usuarioRespuesta.getPassword().equals(password)) {
-                return null;
-            }
-        }
-        usuarioDao.add(usuario);
-        return usuario;
-    }
-
-    @GET
-    @Path("/login/{nombre}/{password}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String login(@PathParam("nombre") String nombre, @PathParam("password") String password) {
-        UsuarioDAO usuarioDao = new UsuarioDAO();
-        Usuario usuario = new Usuario();
-        usuario.setNombre(nombre);
-        usuario.setPassword(password);
-        ArrayList<Usuario> usuarios = usuarioDao.findOne(usuario);
-        for (Usuario usuarioRespuesta : usuarios) {
-            if (!usuarioRespuesta.getNombre().equals(nombre) && !usuarioRespuesta.getPassword().equals(password)) {
-                return null;
-            }
-        }
-
-        return Usuario.fromArrayListToJson(usuarios);
-    }
-
-    @GET
-    @Path("/historialCompras/{idUser}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getHstCompras(@PathParam("idUser") int idUsu) {
-        //TODO return proper representation object
-
-        PedidoDAO pedidoDAO = new PedidoDAO();
-        Pedido pedido = new Pedido();
-        pedido.setId_usuario(idUsu);
-        pedido.setFecha("");
-        pedido.setPrecio_total("");
-        ArrayList<Pedido> lstPedidos = pedidoDAO.findAllAk(pedido);
-        return Pedido.fromArrayListToJson(lstPedidos);
-    }*/
 }
